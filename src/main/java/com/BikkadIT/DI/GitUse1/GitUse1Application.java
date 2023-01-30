d7a97ae745f223c815c4f74b40b34418547626bc
vikaskhare0905 <vikaskharevk1994@gmail.com>
2023-01-30 15:38:38 +0530

    changes by vikas khare

---
+++ src/main/java/com/BikkadIT/DI/GitUse1/GitUse1Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.DI.GitUse1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUse1Application.class, args);
+	}
+
+}
